271211d320f0c2c78a8d0fe50b9ee7695295f63b
khaled759 <khalednabil759@gmail.com>
2026-08-31 05:01:11 +0300

    adding Driver and sequencer mandatory code

---
+++ tb/cfs_apb_driver.sv
@@ -0,0 +1,32 @@
+`ifndef CFS_APB_DRIVER_SV
+    `define CFS_APB_DRIVER_SV
+
+    class cfs_apb_driver extends uvm_driver#(.REQ(cfs_apb_item_drv));
+        
+        `uvm_component_utils(cfs_apb_driver)
+
+        function new(string name = "", uvm_component parent);
+            super.new(name, parent);
+        endfunction
+
+
+        virtual task run_phase(uvm_phase phase);
+
+            forever begin
+                // the item which we will drive to the dut
+                cfs_apb_item_drv item;
+
+                seq_item_port.get_next_item(item);
+
+                // the driving logic
+                `uvm_info("DEBUG", $sformatf("Driving \"%0s\" : %0s", item.get_full_name(), item.convert2string()), UVM_NONE)
+
+                // mark item as done
+                seq_item_port.item_done();
+            end
+
+        endtask
+
+    endclass 
+
+`endif 
--- tb/cfs_apb_pkg.sv
+++ tb/cfs_apb_pkg.sv
@@ -12,7 +12,10 @@
         `include "cfs_apb_item_base.sv"
         `include "cfs_apb_item_drv.sv"
         `include "cfs_apb_agent_config.sv"
+        `include "cfs_apb_sequencer.sv"
+        `include "cfs_apb_driver.sv"
         `include "cfs_apb_agent.sv"
+        
     endpackage
 
 `endif
